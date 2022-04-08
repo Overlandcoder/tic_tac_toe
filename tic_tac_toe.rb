51de6d8e3904eb991921a33b4bcd823aca96e1e7
Overlander <anuragnandal@hotmail.com>
2022-04-08 19:22:46 -0400

    Move logic of checking for invalid symbol into separate method

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -28,6 +28,10 @@ WINNING_COMBINATIONS = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [
     player1.symbol = @symbol
     puts "\n#{player1.name} will be #{@symbol}."
 
+    invalid_symbol?
+  end
+
+  def invalid_symbol?
     if symbol != "X" && symbol != "O"
       puts "Invalid symbol."
       choose_symbol
