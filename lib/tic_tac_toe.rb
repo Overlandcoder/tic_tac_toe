29d9c422f6b0b41b743360bf2916560156b79165
Overlander <anuragnandal@hotmail.com>
2022-05-10 19:56:28 -0400

    Refactor multiple methods for easier testing of game

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,27 +1,28 @@
 class Game
-  attr_reader :current_player, :player1, :player2, :symbol
+  attr_reader :current_player, :player1, :player2
 
   WINNING_COMBINATIONS = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]].freeze
 
   def initialize
     @positions = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    display_board
-    introduction
-    create_players
-    randomize_first_turn
-    play_game
   end
 
-  def introduction
+  def setup
+    display_board
+    intro_message
+    create_players
+    randomize_first_turn
+  end
+
+  def intro_message
     puts "\nLet's play Tic Tac Toe!"
     puts "\nPlayers can make a move by entering a number between 1 and 9. Let's get started."
   end
 
   def create_players
-    @player1 = Player.new(choose_name(1))
-    choose_symbol
-    @player2 = Player.new(choose_name(2))
-    player2.symbol = assign_symbol
+    @player1 = Player.new(choose_name(1), choose_symbol)
+    puts "\n#{player1.name} will be #{player1.symbol}."
+    @player2 = Player.new(choose_name(2), assign_symbol)
     puts "\n#{player2.name} will be #{player2.symbol}."
   end
 
@@ -32,17 +33,16 @@ class Game
 
   def choose_symbol
     puts "\n#{@name}, choose your symbol (X or O):"
-    @symbol = gets.chomp.capitalize
-    player1.symbol = @symbol
-    puts "\n#{player1.name} will be #{@symbol}."
-    invalid_symbol?
+    symbol = gets.chomp.capitalize
+    return symbol unless invalid_symbol?(symbol)
+
+    choose_symbol
   end
 
-  def invalid_symbol?
+  def invalid_symbol?(symbol)
     return unless symbol != 'X' && symbol != 'O'
 
     puts 'Invalid symbol.'
-    choose_symbol
   end
 
   def assign_symbol
@@ -58,11 +58,12 @@ class Game
   end
 
   def play_game
+    setup
+
     until game_over?
       @current_player = switch_turns
       display_board
-      solicit_move
-      mark(@move)
+      mark(solicit_move)
     end
     display_board
     puts "\n#{current_player.name} wins!" if game_won?
@@ -71,22 +72,29 @@ class Game
 
   def solicit_move
     puts "\n#{current_player.name}, make your move."
-    @move = gets.chomp.to_i
+    move = gets.chomp.to_i
+    return move if valid_move?(move)
+
+    solicit_move
   end
 
   def mark(position)
-    if valid_move?
-      @positions[position - 1] = current_player.symbol
-    else
-      puts 'Invalid move, please try again.'
-      solicit_move
-      mark(@move)
-    end
+    @positions[position - 1] = current_player.symbol
   end
 
   def game_won?
+    triple_x? || triple_o?
+  end
+
+  def triple_x?
     WINNING_COMBINATIONS.any? do |combination|
-      combination.all? { |position| @positions[position] == current_player.symbol }
+      combination.all? { |position| @positions[position] == 'X' }
+    end
+  end
+
+  def triple_o?
+    WINNING_COMBINATIONS.any? do |combination|
+      combination.all? { |position| @positions[position] == 'O' }
     end
   end
 
@@ -95,11 +103,13 @@ class Game
   end
 
   def game_over?
-    game_won? || game_tied?
+    game_won?|| game_tied?
   end
 
-  def valid_move?
-    return true if @positions[@move - 1].is_a? Integer
+  def valid_move?(move)
+    return true if @positions[move - 1].is_a? Integer
+
+    puts 'Invalid move, please try again.'
   end
 
   def display_board
@@ -112,11 +122,11 @@ class Game
 end
 
 class Player
-  attr_reader :name
-  attr_accessor :symbol
+  attr_reader :name, :symbol
 
-  def initialize(name)
+  def initialize(name, symbol)
     @name = name
+    @symbol = symbol
   end
 end
 
