5bf501b61012d9bd607c03fb71e5ba5108cd1cf5
Overlander <anuragnandal@hotmail.com>
2022-05-26 15:01:47 -0400

    Refactor tests for #play_game

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,4 +1,4 @@
-require './lib/game.rb'
+require_relative '../lib/game'
 
 describe Game do
   subject(:game) { described_class.new }
@@ -155,58 +155,17 @@ describe Game do
 
   describe '#play_game' do
     let(:player1) { double(Player, name: 'John', symbol: 'X') }
-    let(:player2) { double(Player, name: 'Jake', symbol: 'O') }
-    
-    context 'when game_over? is false once' do
-      before do
-        allow(game).to receive(:setup)
-        allow(game).to receive(:game_over?).and_return(false, true)
-        allow(game).to receive(:puts)
-      end
-
-      it 'calls #switch_turns once' do
-        expect(game).to receive(:switch_turns).once
-        game.play_game
-      end
-
-      before do
-        allow(game).to receive(:solicit_move)
-        allow(game).to receive(:switch_turns)
-        allow(game).to receive(:mark)
-      end
-
-      it 'calls #display_board once' do
-        expect(game).to receive(:display_board).twice
-        game.play_game
-      end
-
-      it 'calls #mark once' do
-        expect(game).to receive(:mark).once
-        game.play_game
-      end
-
-      it 'calls #solicit_move once' do
-        expect(game).to receive(:solicit_move).once
-        game.play_game
-      end
-    end
+    let(:player2) { double(Player, name: 'Jane', symbol: 'O') }
 
     context 'when game_over? is false five times' do
       before do
         allow(game).to receive(:setup)
         allow(game).to receive(:game_over?).and_return(false, false, false, false, false, true)
         allow(game).to receive(:puts)
-      end
-
-      it 'calls #switch_turns five times' do
-        expect(game).to receive(:switch_turns).exactly(5).times
-        game.play_game
-      end
-
-      before do
-        allow(game).to receive(:solicit_move)
-        allow(game).to receive(:switch_turns)
-        allow(game).to receive(:mark)
+        allow(game).to receive(:solicit_move).and_return(1)
+        game.instance_variable_set(:@player1, player1)
+        game.instance_variable_set(:@player2, player2)
+        game.instance_variable_set(:@current_player, player1)
       end
 
       it 'calls #display_board six times' do
@@ -219,8 +178,14 @@ describe Game do
         game.play_game
       end
 
-      it 'calls #solicit_move five times' do
-        expect(game).to receive(:solicit_move).exactly(5).times
+      it 'calls #switch_turns five times' do
+        allow(game).to receive(:current_player).and_return(player1)
+        expect(game).to receive(:switch_turns).exactly(5).times
+        game.play_game
+      end
+
+      it 'calls #conclusion once' do
+        expect(game).to receive(:conclusion).once
         game.play_game
       end
     end
